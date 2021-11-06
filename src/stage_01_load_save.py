648f0a51c4968f09a118dcf967300df9a2a7f931
siddharth rana <sidhu.rana34@gmail.com>
2021-11-06 16:21:03 +0530

    logging added and stage 1 done

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -8,7 +8,7 @@ import logging
 
 logging_str ="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir ="logs"
-create_directory([log_dir])
+os.makedirs(log_dir,exist_ok=True)
 logging.basicConfig(filename = os.path.join(log_dir,"running_logs.log"),level = logging.INFO,format=logging_str,
 filemode='a')
 
